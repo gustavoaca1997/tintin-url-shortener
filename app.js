d80bb1ec5824410571f50c4f0c17ceba1e9482bf
Gus <gustavoaca1997@gmail.com>
2017-12-26 21:06:56 -0400

    Use valid-url package to validate URL

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // packages
 const MongoClient = require('mongodb').MongoClient;
-const encode = require('hashcode').hashCode;
 const express = require('express');
+const validUrl = require('valid-url');
 // app
 const app = express();
 const PORT = process.env.PORT || 1337;
@@ -44,68 +44,54 @@ app.get('/new/:URL*', function(req, res) {
 	var original = req.url.slice(5);
 	console.log("Original URL: " + original);
 	var retObj = {"original": original};
-	validUrl(original, function(err) {
-		if (err) {
-			retObj = {err: "Invalid URL"};
-			console.log("Invalid URL passed.")
-			res.json(retObj);
-		}
-		else {
-			MongoClient.connect(url, function(err, client) {
+	
+	if (!validUrl.isWebUri(original)) {
+		retObj = {err: "Invalid URL"};
+		console.log("Invalid URL passed.")
+		res.json(retObj);
+	}
+	else {
+		MongoClient.connect(url, function(err, client) {
+			if (err) throw err;
+			let db = client.db(dbName);
+			let collection = db.collection("originals");
+			collection.find(retObj, {projection: {_id: 0}}).toArray(function(err, result) {
 				if (err) throw err;
-				let db = client.db(dbName);
-				let collection = db.collection("originals");
-				collection.find(retObj, {projection: {_id: 0}}).toArray(function(err, result) {
-					if (err) throw err;
-					if (result.length > 0) {
-						retObj = result[0];
-						res.json(retObj)
-					}
-					else {
-						let cont;
-						do {
-							let digits = Math.floor(Math.random()*6+1);
-							let short = Math.floor(Math.random()*1000000+1).toString().slice(0,digits);
-							collection.findOne({"short": short}, function(err, result) {
-								if (err) throw err;
-								if (result) cont = true;
-								else {
-									retObj = {
-										"original": original, 
-										"short": link_app+short
-									};
-									//console.log("retObj: " + JSON.stringify(retObj));
-									collection.insertOne(retObj, function(err, data) {
-										if (err) throw err;
-										delete retObj._id;
-										res.json(retObj);
-										client.close();
-									});
-									cont = false;
-								}
-							});
-						} while(cont);
-					}
-				})
-			});
-		}
-	});
+				if (result.length > 0) {
+					retObj = result[0];
+					res.json(retObj)
+				}
+				else {
+					let cont;
+					do {
+						let digits = Math.floor(Math.random()*6+1);
+						let short = Math.floor(Math.random()*1000000+1).toString().slice(0,digits);
+						collection.findOne({"short": short}, function(err, result) {
+							if (err) throw err;
+							if (result) cont = true;
+							else {
+								retObj = {
+									"original": original, 
+									"short": link_app+short
+								};
+								//console.log("retObj: " + JSON.stringify(retObj));
+								collection.insertOne(retObj, function(err, data) {
+									if (err) throw err;
+									delete retObj._id;
+									res.json(retObj);
+									client.close();
+								});
+								cont = false;
+							}
+						});
+					} while(cont);
+				}
+			})
+		});
+	}
+	
 });
 
 app.listen(PORT, function() {
 	console.log("Listening on port " + PORT);
 });
-
-// functions
-function validUrl(original, callback) {
-	//http://www.example.com
-	let aux = original.split('://');
-	if ((aux[0] != "http" && 
-		aux[0] != "https") ||
-		aux[1].split('.').length < 2) {
-		callback(true);
-	}
-	else {
-		callback(false);
-	}
-}
